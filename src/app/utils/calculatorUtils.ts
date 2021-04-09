f7f3848cbdf978ffc4cc6cfd3311caaf117bd767
ToreDenStore <jonatan.sjo.jerling@gmail.com>
2021-04-09 20:50:32 +0200

    Code works but gets stack size error

--- src/app/utils/calculatorUtils.ts
+++ src/app/utils/calculatorUtils.ts
@@ -1,13 +1,23 @@
+import { max, sum } from 'mathjs';
 import { PerformancePoint, PerformanceSeries } from '../models/performance-series';
 import { PortfolioCalculation, PortfolioHolding } from '../models/portfolio-calculation';
 import { PerformanceUtils } from './performanceUtils';
 
 export class CalculatorUtils {
 
+    private static loopStopper = 0;
+    private static loopStop = 25000;
+
     public static optimizeSharpe(listOfSeries: PerformanceSeries[]): PortfolioCalculation {
-        const weights: number[] = [0.01, 0.1, 0.2, 0.5, 1, 2, 5, 10, 100];
+        const weights: number[] = [0.001, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 100, 1000];
         const calculations: PortfolioCalculation[] = [];
+        let calculation: PortfolioCalculation;
         let maxSharpe = -99;
+
+        const numberOfWeights = weights.length;
+        const numberOfSeries = listOfSeries.length;
+        const numberOfSimulations = Math.pow(numberOfWeights, numberOfSeries);
+        console.log('Number of simulations: ' + numberOfSimulations);
 
         if (listOfSeries.length === 3) {
             console.log('Optimizing...');
@@ -45,23 +55,128 @@ export class CalculatorUtils {
                     }
                 }
             }
+
+            calculation = calculations.find(x => {
+                return x.sharpeRatio === maxSharpe;
+            });
+        } else {
+            const run = new RecursiveRun();
+            // run.calculations = calculations;
+            run.listOfSeries = listOfSeries;
+            run.weights = weights;
+            run.indexToIncrement = 0;
+            run.holdingWeights = [];
+            listOfSeries.forEach(_ => {
+                run.holdingWeights.push(0);
+            });
+
+            const result: RecursiveRun = this.doRecursiveRun(run);
+            // console.log('Recursive run finished! Result has number of calculations: ' + result.calculations.length);
+            // calculations = result.calculations;
+
+            // calculations.forEach(calc => {
+            //     if (calc.sharpeRatio > maxSharpe) {
+            //         maxSharpe = calc.sharpeRatio;
+            //     }
+            // });
+
+            maxSharpe = result.maxSharpe;
+
+            console.log('Min stdev: ' + result.minStdev);
+
+            calculation = result.calculationMaxSharpe;
         }
 
         console.log('Best sharpe: ' + maxSharpe);
 
-        const bestCalc = calculations.find(x => {
-            return x.sharpeRatio === maxSharpe;
-        });
-
         // console.log('Best calc: ' + JSON.stringify(bestCalc));
 
-        return bestCalc;
+        return calculation;
+    }
+
+    private static doRecursiveRun(recursiveRun: RecursiveRun): RecursiveRun
+    {
+        CalculatorUtils.loopStopper += 1;
+
+        // console.log('Holdings weight array: ' + JSON.stringify(recursiveRun.holdingWeights));
+
+        if (CalculatorUtils.loopStopper % 100 === 0) {
+            console.log('Loop nr ' + CalculatorUtils.loopStopper);
+        }
+
+        if (CalculatorUtils.loopStopper > CalculatorUtils.loopStop) {
+            console.log('Loop has been stopped, too many loops! Max is ' + CalculatorUtils.loopStop);
+            return null;
+        }
+
+        // Increment holding weight array
+        if (recursiveRun.holdingWeights[recursiveRun.indexToIncrement] === recursiveRun.weights.length) {
+            // Increase higher index by 1
+            recursiveRun.indexToIncrement += 1;
+            recursiveRun.holdingWeights[recursiveRun.indexToIncrement] += 1;
+            // Reset all lower indices
+            for (let index = 0; index < recursiveRun.indexToIncrement; index++) {
+                recursiveRun.holdingWeights[index] = 0;
+            }
+            this.doRecursiveRun(recursiveRun);
+        }
+
+        if (recursiveRun.indexToIncrement >= recursiveRun.listOfSeries.length) {
+            // End loop
+            console.log('Unwind!');
+            return recursiveRun;
+        }
+
+        recursiveRun.indexToIncrement = 0;
+
+        const holdings: PortfolioHolding[] = [];
+        for (let index = 0; index < recursiveRun.listOfSeries.length; index++) {
+            const performanceSerie = recursiveRun.listOfSeries[index];
+            const holding = new PortfolioHolding();
+            holding.performanceSeries = performanceSerie.performanceSeries;
+            holding.ticker = performanceSerie.ticker;
+            const weightArrayIndex = recursiveRun.holdingWeights[index];
+            holding.weight = recursiveRun.weights[weightArrayIndex];
+            holdings.push(holding);
+        }
+
+        const calculation = CalculatorUtils.runPortfolioCalculation(holdings);
+        // console.log('Sharpe ratio for this calc: ' + calculation.sharpeRatio);
+
+        // recursiveRun.calculations.push(calculation);
+
+        // console.log('Run had ' + recursiveRun.calculations.length + ' number of calculations');
+
+        if (calculation.sharpeRatio > recursiveRun.maxSharpe) {
+            recursiveRun.maxSharpe = calculation.sharpeRatio;
+            recursiveRun.calculationMaxSharpe = calculation;
+            console.log('New max sharpe: ' + recursiveRun.maxSharpe);
+        }
+
+        if (calculation.stDev < recursiveRun.minStdev) {
+            recursiveRun.minStdev = calculation.stDev;
+            recursiveRun.calculationMinStdev = calculation;
+            console.log('New min stdev: ' + recursiveRun.minStdev);
+        }
+
+        recursiveRun.holdingWeights[recursiveRun.indexToIncrement] += 1;
+
+        return CalculatorUtils.doRecursiveRun(recursiveRun);
     }
 
     public static runPortfolioCalculation(holdings: PortfolioHolding[]): PortfolioCalculation {
         const days = holdings[0].performanceSeries.length;
         const performances: number[] = [];
         const performancePoints: PerformancePoint[] = [];
+
+        // Normalize weights
+        let totalWeight = 0;
+        holdings.forEach(holding => {
+            totalWeight += holding.weight;
+        });
+        holdings.forEach(holding => {
+            holding.weight *= (1 / totalWeight);
+        });
 
         for (let index = 0; index < days; index++) {
             let performance = 0;
@@ -86,4 +201,15 @@ export class CalculatorUtils {
 
         return calculation;
     }
+}
+
+export class RecursiveRun {
+    calculationMaxSharpe: PortfolioCalculation;
+    calculationMinStdev: PortfolioCalculation;
+    maxSharpe = -99;
+    minStdev = 99;
+    weights: number[];
+    listOfSeries: PerformanceSeries[];
+    holdingWeights: number[];
+    indexToIncrement: number;
 }
